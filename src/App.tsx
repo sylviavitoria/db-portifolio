86bc6199c0723b5eb5f76bb7b6f7e40c028da404
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-07-15 17:25:20 -0300

    feat: adiciona rotas para as páginas Home, Currículo e Projetos no componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Curriculo from './pages/Curriculo';
+import Projetos from './pages/Projetos';
 
 function App() {
   return (
     <Router>
       <Navbar />
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/curriculo" element={<Curriculo />} />
+        <Route path="/projetos" element={<Projetos />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
