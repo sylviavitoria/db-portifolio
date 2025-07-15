cf70e06053d343808bab8aae0c1b440c1d492da9
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-07-15 17:26:29 -0300

    feat: adiciona componente PerfilSection com informações pessoais

---
+++ src/components/PerfilSection.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const PerfilSection = () => {
+  return (
+    <div className="left-content">
+      <div className="greeting">
+        <h2 className="greeting-text">Olá, meu nome é</h2>
+      </div>
+
+      <div className="name-section">
+        <h1 className="name">Sylvia Vitória</h1>
+        <h2 className="position">
+          Sou trainee de desenvolvimento de software na DBServer
+        </h2>
+      </div>
+
+      <div className="about-section">
+        <h1 className="section-title">Sobre mim</h1>
+        <h2 className="about-text">
+          Sou Sylvia Vitória, trainee em desenvolvimento de software na DBServer e estudante do 6º período de Sistemas de Informação. 
+          Tenho conhecimento em linguagens como Java e JavaScript/TypeScript, além de experiência com frameworks e ferramentas como Spring Boot, React, HTML5, CSS3, Swagger e Postman.
+          Sou apaixonada por tecnologia e comprometida com a entrega de código de qualidade, seguindo boas práticas como os princípios S.O.L.I.D e Clean Code. 
+          Tenho um perfil proativo, criativo e focado em aprendizado contínuo. Busco uma oportunidade que impulsione meu crescimento profissional e me permita evoluir como desenvolvedora, contribuindo com soluções eficientes, escaláveis e inovadoras.
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default PerfilSection;
