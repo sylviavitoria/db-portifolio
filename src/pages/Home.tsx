a19744ef9ea84ff98446ab4792e8072edd6e99fa
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-07-15 17:27:09 -0300

    feat: cria componente Home com seções de perfil, foto e habilidades

---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PerfilSection from '../components/PerfilSection';
+import FotoSection from '../components/FotoSection';
+import HabilidadesSection from '../components/HabilidadesSection';
+
+const Home = () => {
+  return (
+    <div className="sobre-container">
+      <div className="main-content">
+        <PerfilSection />
+        
+        <div className="right-content">
+          <FotoSection />
+          <HabilidadesSection />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
