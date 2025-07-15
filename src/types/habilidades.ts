90820d2ffc3a536fb1ae369cb69be85c2e8453dd
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-07-15 17:26:57 -0300

    feat: adiciona seção de habilidades com componente Habilidade e dados de habilidades

---
+++ src/types/habilidades.ts
@@ -0,0 +1,18 @@
+export const habilidades = {
+  'Backend': [
+    { titulo: 'JAVA', nivel: 4 },
+    { titulo: 'SPRING BOOT', nivel: 3 },
+    { titulo: 'POSTGRES SQL/MYSQL', nivel: 4 },
+    { titulo: 'POSTMAN', nivel: 5 },
+    { titulo: 'OpenAPI/SWAGGER', nivel: 5 },
+    { titulo: 'DOCKER', nivel: 3 },
+    { titulo: 'FLYWAY', nivel: 4 }, 
+  ],
+  'Frontend': [
+    { titulo: 'HTML', nivel: 5 },
+    { titulo: 'CSS', nivel: 4 },
+    { titulo: 'JAVASCRIPT/ TYPESCRIPT', nivel: 3 },
+    { titulo: 'REACT', nivel: 3 },
+    { titulo: 'REACT NATIVE', nivel: 3 },
+  ],
+};
