5a9857b4031d1e51a6343e7d229c2429b593543a
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-07-15 17:26:43 -0300

    feat: adiciona componente FotoSection com imagem de perfil

---
+++ src/components/FotoSection.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import fotodb from '../assets/portifolio-foto.jpg';
+
+const FotoSection = () => {
+  return (
+    <div className="photo-section">
+      <img
+        src={fotodb}
+        alt="Foto"
+        className="profile-photo"
+      />
+    </div>
+  );
+};
+
+export default FotoSection;
