06413553461c26dc0406f9a1b1e9f5deb41842b3
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-07-15 17:26:07 -0300

    feat: adiciona componente Footer com logo

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../assets/d-b-dark-blue-horizontal.png';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="navbar-left">
+        <img src={logo} alt="Logo DB" className="logo" />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
