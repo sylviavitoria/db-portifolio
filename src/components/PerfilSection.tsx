061c36b862ca63f0568ad4b3408ec63f56f05715
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-07-16 19:51:17 -0300

    docs: atualizar ano de início da jornada na tecnologia na seção "Sobre mim"

--- src/components/PerfilSection.tsx
+++ src/components/PerfilSection.tsx
@@ -17,7 +17,7 @@ const PerfilSection = () => {
       <div className="about-section">
         <h1 className="section-title">Sobre mim</h1>
         <h2 className="about-text">
-          Estudante do 6º período de Sistemas de Informação, minha jornada na área de tecnologia começou em 2022 e tem sido marcada por um aprendizado constante e pela busca por desafios que me façam crescer.
+          Estudante do 6º período de Sistemas de Informação, minha jornada na área de tecnologia começou em 2023 e tem sido marcada por um aprendizado constante e pela busca por desafios que me façam crescer.
           Tenho conhecimento em linguagens como Java e JavaScript/TypeScript, além de experiência com frameworks e ferramentas como Spring Boot, React, HTML5, CSS3, Swagger e Postman.
           Sou apaixonada por tecnologia e comprometida com a entrega de código de qualidade, seguindo boas práticas como os princípios S.O.L.I.D e Clean Code. 
           Tenho um perfil proativo, criativo e focado em aprendizado contínuo. Busco uma oportunidade que impulsione meu crescimento profissional e me permita evoluir como desenvolvedora, contribuindo com soluções eficientes, escaláveis e inovadoras.
