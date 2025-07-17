2bea0a1aa9a82531b926bfc112cf456d473a0f26
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-07-17 11:49:56 -0300

    refactor: remover configuração de TextEncoder e TextDecoder do setup de testes

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,11 +1,1 @@
 import '@testing-library/jest-dom';
-
-import { TextEncoder, TextDecoder } from 'util';
-
-if (typeof global.TextEncoder === 'undefined') {
-  global.TextEncoder = TextEncoder;
-}
-
-if (typeof global.TextDecoder === 'undefined') {
-  global.TextDecoder = TextDecoder;
-}
